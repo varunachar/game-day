e98d91131ae8b466243bd940149382be036285d2
Varun Achar <varun.achar@razorpay.com>
2021-01-21 15:47:03 +0530

    Added auto reload to node

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', function(req, res, next) {
+  var name = req.body.name;
+  var email = req.body.email;
+  var mobile = req.body.mobile;
+
+  res.send('Received name=' + name + " " + ", email=" + email + ", mobile=" + mobile);
 });
 
 module.exports = router;
