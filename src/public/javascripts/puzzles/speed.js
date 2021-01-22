9854d52d49ffbaa7c6547e066d2fe73984b38825
Varun Achar <varun.achar@razorpay.com>
2021-01-22 16:12:28 +0530

    Changes the text message for speed.sh

--- src/public/javascripts/puzzles/speed.js
+++ src/public/javascripts/puzzles/speed.js
@@ -1,4 +1,4 @@
 function speed() {
-    term.echo("\n\n\nOh yeah? You now have only 3 mins to finish the game\n\n\n");
+    term.echo("\n\n\nOh yeah? You've just lost 3 mins from the clock.\n\n\n");
     // Add timer
 }
